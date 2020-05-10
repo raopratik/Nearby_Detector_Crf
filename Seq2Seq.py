06fadd8a58d755033b31eb82653fd5c097a6a348
pratik <pjayarao@andrew.cmu.edu>
2020-05-10 00:03:04 -0400

    copy code from recitation for seq2seq

---
+++ Seq2Seq.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from Attention import Attention
+from constants import DEVICE
+
+
+class Seq2Seq(nn.Module):
+    '''
+    We train an end-to-end sequence to sequence model comprising of Encoder and Decoder.
+    This is simply a wrapper "model" for your encoder and decoder.
+    '''
+    def __init__(self, input_dim, vocab_size, hidden_dim, value_size=128, key_size=128, isAttended=True):
+        super(Seq2Seq, self).__init__()
+        self.encoder = Encoder(input_dim, hidden_dim)
+        self.decoder = Decoder(vocab_size, hidden_dim)
+
+    def forward(self, speech_input, speech_len, text_input=None, isTrain=True, epoch=None):
+        key, value, lens = self.encoder(speech_input, speech_len, isTrain)
+        if (isTrain == True):
+            predictions = self.decoder(key, value, lens, text_input, epoch=epoch)
+        else:
+            predictions = self.decoder(key, value, lens, text=None, isTrain=False, epoch=None)
+        return predictions
